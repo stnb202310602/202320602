d1bd6766a50fa9870349689a88a476d21baaac7d
stnb202310602 <tjdruddl12@gmail.com>
2023-09-18 16:30:01 +0900

    230918 프밍 과제

--- 20230912프밍/230912프밍과제-1.py
+++ 20230912프밍/230912프밍과제-1.py
@@ -1,5 +1,10 @@
-def rev_str(str):
-    return rev_str(str[1:]) + str[0]
+def rev_str(s):
+    # 종료 조건: 문자열이 비어있을 때
+    if len(s) == 0:
+        return s
+    else:
+        # 문자열의 첫 번째 문자와 나머지 문자열을 뒤집어 합칩니다.
+        return rev_str(s[1:]) + s[0]
 
 if __name__ == '__main__':
     print(rev_str('ABCDE'))
--- 20230912프밍/230912프밍과제3.py
+++ 20230912프밍/230912프밍과제3.py
@@ -1,29 +1,14 @@
 import re
-
-search_text = input("Enter an email what you will find.")
-at_index = search_text.find('@')
-if at_index != -1:
-    domain = search_text[at_index + 1:]
-else:
-    print("Worng email form")
-    exit()
-
-file_path = input("Enter path")
-
-try:
-    with open(file_path, 'r', encoding='utf-8') as file:
-        file_contents = file.read()
-
-        pattern = f'\\b\\w+@{re.escape(domain)}\\b'
-        matches = re.findall(pattern, file_contents)
-
-        if matches:
-            print(f"email domain: {domain}")
-            for match in matches:
-                print(match)
-        else:
-            print(f"I couldn't find {domain} in the text file.")
-except FileNotFoundError:
-    print(f"I couldn't find '{file_path}'")
-except Exception as e:
-    print(f"Error: {str(e)}")
+input_text = input("이메일을 스페이스바로 구분하여 입력하세요: ")
+email_addresses = [email.strip() for email in input_text.split()]
+email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b')
+found = False
+print("찾은 이메일:")
+for email_address in email_addresses:
+    matches = email_pattern.findall(email_address)
+    if matches:
+        found = True
+        for match in matches:
+            print(match)
+if not found:
+    print("이메일이 발견되지 않았습니다.")
